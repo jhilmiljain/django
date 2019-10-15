695108a2d58a00cb02b2ec6078364c98c1efddf5
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-10-16 00:31:16 +0530

    Update models.py

--- models.py
+++ models.py
@@ -64,7 +64,7 @@ class User(AbstractBaseUser):
     def is_active(self):
         return self.active
     
-class GuesttEmail(models.Model):
+class GuestEmail(models.Model):
     email     = models.EmailField()
     active    = models.BooleanField(default= True)
     update    = models.DateTimeField(auto_now= True)
